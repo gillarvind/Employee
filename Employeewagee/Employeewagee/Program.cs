02323ab8057786f7eec80e06322c029cf0a834b6
Arvind Gill <arvindkhtmuz@gmail.com>
2022-09-28 17:22:35 +0530

    [arvind] Add:folder structure creates

---
+++ Employeewagee/Employeewagee/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int IS_FULL_TIME = 1;
+        Random random = new Random();
+        int empCheck = random.Next();
+        if (empCheck == IS_FULL_TIME)
+        {
+            Console.WriteLine("present");
+        }
+        else
+        {
+            Console.WriteLine("absent");
+        }
+    }
+}
